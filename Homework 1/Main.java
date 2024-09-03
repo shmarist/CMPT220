0352eb96d713f897e92d6fd97780d4d311bfa8e1
shmarist <samuel.hornstein1@marist.edu>
2024-09-03 13:47:45 -0400

    hw 1

--- Homework 1/Main.java
+++ Homework 1/Main.java
@@ -18,7 +18,7 @@ public class Main {
         //finish this one for me by receiving and printing the user's age back to them
         System.out.println("How old are you? ");
         String age = sc.nextLine();
-        System.out.println("You are " + age + "years old.");
+        System.out.println("You are " + age + " years old.");
 
         //can you do some math for me? take these variable and print out the sum of them!
         int num1 = 10;
